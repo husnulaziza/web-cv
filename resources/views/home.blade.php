ef4c9d460a804b2515056507a4ad535352ea35ba
khusnulaziza <khusnulaziza18@gmail.com>
2022-12-11 17:46:00 +0700

    menambahkan commit file

---
+++ resources/views/home.blade.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Resume/CV Design</title>
+	<link rel="stylesheet" href="{{asset('stylescv.css')}}">
+  <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+
+<div class="resume">
+   <div class="resume_left">
+     <div class="resume_profile">
+       <img src="{{asset('foto.jpeg') }}" alt="profile_pic">
+     </div>
+     <div class="resume_content">
+       <div class="resume_item resume_info">
+         <div class="title">
+           <p class="bold">khusnul aziza</p>
+         </div>
+         <ul>
+           <li>
+             <div class="icon">
+               <i class="fas fa-map-signs"></i>
+             </div>
+             <div class="data">
+               Kota Banda Aceh <br /> Indonesia
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fas fa-mobile-alt"></i>
+             </div>
+             <div class="data">
+               +6282333333333
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fas fa-envelope"></i>
+             </div>
+             <div class="data">
+               khusnulaziza18@gmail.com
+             </div>
+           </li>
+           
+         </ul>
+       </div>
+       <div class="resume_item resume_skills">
+         <div class="title">
+           <p class="bold">skill's</p>
+         </div>
+         <ul>
+           <li>
+             <div class="skill_name">
+               JAVA
+             </div>
+             <div class="skill_progress">
+               <span style="width: 80%;"></span>
+             </div>
+             <div class="skill_per">80%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               Typography
+             </div>
+             <div class="skill_progress">
+               <span style="width: 70%;"></span>
+             </div>
+             <div class="skill_per">70%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               Figma
+             </div>
+             <div class="skill_progress">
+               <span style="width: 50%;"></span>
+             </div>
+             <div class="skill_per">50%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               Photoshop
+             </div>
+             <div class="skill_progress">
+               <span style="width: 40%;"></span>
+             </div>
+             <div class="skill_per">40%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               PHP
+             </div>
+             <div class="skill_progress">
+               <span style="width: 60%;"></span>
+             </div>
+             <div class="skill_per">60%</div>
+           </li>
+         </ul>
+       </div>
+       <div class="resume_item resume_skills">
+         <div class="title">
+           <p class="bold">Social</p>
+         </div>
+         <ul>
+          <li>
+            <li>      
+            <div class="skill_name">
+                    Indonesia
+                  </div>
+                  <div class="skill_progress">
+                    <span style="width: 100%;"></span>
+                  </div>
+                  <div class="skill_per">100%</div>
+                </li>
+              <li>
+                <div class="skill_name">
+                  English
+                </div>
+                <div class="skill_progress">
+                  <span style="width: 30%;"></span>
+                </div>
+                <div class="skill_per">30%</div>
+              </li>
+              <li>
+                <div class="skill_name">
+
+        </ul>
+       </div>
+     </div>
+  </div>
+  <div class="resume_right">
+    <div class="resume_item resume_about">
+        <div class="title">
+           <p class="bold">About us</p>
+         </div>
+        <p>My Name is khusnul aziza, I'm 20 Years Old, I From Sigli, Indonesia</p>
+        <p>I student from State Islamic University of Ar-Raniry From Major Information Technologi</p>
+        <p>My Hoby is Watching movie</p>
+    </div>
+    <div class="resume_item resume_work">
+        <div class="title">
+           <p class="bold">Education</p>
+         </div>
+        <ul>
+            <li>
+                <div class="date">2006-2012</div> 
+                <div class="info">
+                     <p class="semi-bold">MIN Tanoh Anoe jangaka</p> 
+                  
+                </div>
+            </li>
+            <li>
+              <div class="date">2013-2016</div>
+              <div class="info">
+                     <p class="semi-bold">SMP it muhammadiyah gandapura</p> 
+                  
+                </div>
+            </li>
+            <li>
+              <div class="date">2017 - 2020</div>
+              <div class="info">
+                     <p class="semi-bold">MAS muhammadiyah boarding school Bireuen</p> 
+                </div>
+            </li>
+            <li>
+              <div class="date">2020 - 2024</div>
+              <div class="info">
+                     <p class="semi-bold">UIN Ar-Raniry, Banda Aceh</p> 
+                </div>
+            </li>
+            
+          </li>
+        </ul>
+    </div>
+ 
+    <div class="resume_item resume_hobby">
+      <div class="title">
+           <p class="bold">Hobby</p>
+         </div>
+       <ul>
+         <li><i class="fas fa-book"></i></li>
+         <li><i class="fas fa-gamepad"></i></li>
+         <li><i class="fas fa-music"></i></li>
+         <li><i class="fas fa-basketball-ball"></i></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
